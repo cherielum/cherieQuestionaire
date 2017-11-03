f3403bb7e81997d6179fd21b0db96fa23be2eaa2
Cherie Lum <charismatic.cherie@gmail.com>
2017-11-03 15:22:27 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,110 @@
+function myFunction() {
+	document.getElementById("q1").innerHTML = "Chicken Tikki Masala."
+	// document.getElementById("q1").style.color = "red";
+	document.getElementById("food").style.color = "red";
+	document.getElementById("title").style.color = "red";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack() {
+	document.getElementById("q1").innerHTML = "What is your favorite dish?"
+	document.body.style.color = "black";
+}
+
+
+function myFunction2() {
+	document.getElementById("q2").innerHTML = "Neon-Blue."
+	// document.getElementById("q2").style.color = "#42d9f4";
+	document.getElementById("color").style.color = "#42d9f4";
+	document.getElementById("title").style.color = "#42d9f4";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack2() {
+	document.getElementById("q2").innerHTML = "What is your favorite color?"
+	document.body.style.color = "black";
+}
+
+function myFunction3() {
+	document.getElementById("q3").innerHTML = "Playing Pokemon Go."
+	// document.getElementById("q3").style.color = "#ef3eef";
+	document.getElementById("hobby").style.color = "#ef3eef";
+	document.getElementById("title").style.color = "#ef3eef";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack3() {
+	document.getElementById("q3").innerHTML = "What is one weird hobby you have?"
+	document.body.style.color = "black";
+}
+function myFunction4() {
+	document.getElementById("q4").innerHTML = "He was 6'1\"."
+	// document.getElementById("q4").style.color = "pink";
+	document.getElementById("romance").style.color = "pink";
+	document.getElementById("title").style.color = "pink";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack4() {
+	document.getElementById("q4").innerHTML = "How tall was your first crush?"
+	document.body.style.color = "black";
+}
+function myFunction5() {
+	document.getElementById("q5").innerHTML = "Christmas!" 
+	document.getElementById("holiday").style.color = "#69e50b";
+	document.getElementById("title").style.color = "#69e50b";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack5() {
+	document.getElementById("q5").innerHTML = "What is your favorite holiday?"
+	document.body.style.color = "black";
+}
+function myFunction6() {
+	document.getElementById("q6").innerHTML = "Twenty-eight and counting!"
+	document.getElementById("travel").style.color = "green";
+	document.getElementById("title").style.color = "green";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack6() {
+	document.getElementById("q6").innerHTML = "How many states have you been to?"
+	document.body.style.color = "black";
+}
+function myFunction7() {
+	document.getElementById("q7").innerHTML = "One time someone decorated a Four Seasons Hotel Suite with a bouquet of huge blue balloons, roses, and variety of chips for me!"
+	document.getElementById("romance2").style.color = "pink";
+	document.getElementById("title").style.color = "pink";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack7() {
+	document.getElementById("q7").innerHTML = "What is the most romantic thing anybody has ever done?"
+	document.body.style.color = "black";
+}
+
+function myFunction8() {
+	document.getElementById("q8").innerHTML = "Anything my grandpa would make. He raised me and I loved his cooking. But he passed away in late 2016 and nobody cooks quite like him! If I were to be more realistic though, probably my mom's soy sauce chicken wings cooked Cantonese Style and some salty duck eggs. This would all be on a bed of rice."
+	document.getElementById("food2").style.color = "red";
+	document.getElementById("title").style.color = "red";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack8() {
+	document.getElementById("q8").innerHTML = "What would you eat for your \"last meal\" and why?"
+	document.body.style.color = "black";
+}
+function myFunction9() {
+	document.getElementById("q9").innerHTML = "I would have to say Italy for pasta, Korea for spicy chickn, and Japan for sushi. However if I had to choose one place to live forever, it'd be the U.S.A."
+	document.getElementById("travel2").style.color = "green";
+	document.getElementById("title").style.color = "green";
+	document.body.style.backgroundColor= "black";
+}
+
+function myFunctionBack9() {
+	document.getElementById("q9").innerHTML = "Where is your favorite country in the whole world?"
+	document.body.style.color = "black";
+}
+
+
+
